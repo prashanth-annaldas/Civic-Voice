c55a7b68067fe90e315e43b886f559116d773d67
prashanth <prashanthannaldas453@gmail.com>
2025-12-28 00:15:28 +0530

    Prashanth_Updated

---
+++ civic_voice_frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import NavBar from './components/navBar';
+import NavBarHome from './components/navBarHome.jsx'
+import Register from './components/register';
+import Login from './components/login';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet } from 'react-router-dom';
+import Location from './components/geoLocation.jsx';
+
+function App() {
+  return (
+    <>
+    <NavBarHome />
+    <Outlet />
+    </>
+    );
+}
+//  navBarTab = {navBarTab} setNavBarTab = {setNavBarTab}
+export default App;
