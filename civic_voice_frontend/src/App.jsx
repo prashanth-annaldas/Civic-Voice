5d3a9f4e66df991dac8aa8deacd1ebdc8b79a5c8
prashanth <prashanthannaldas453@gmail.com>
2026-01-07 21:29:30 +0530

    Prashanth Updated

--- civic_voice_frontend/src/App.jsx
+++ civic_voice_frontend/src/App.jsx
@@ -1,21 +1,10 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import NavBarHome from "./components/navBarHome";
-import NavBar from "./components/navBar";
 
 function App() {
-  const location = useLocation();
-  const showNav = location.pathname === "/";
-  const showNavHome = location.pathname === "/home" || location.pathname === "/problems" || location.pathname === "/requests" || location.pathname === "/admin" || location.pathname === "/about";
 
-  return (
-    <>
-      {showNav && <NavBar />}
-      {showNavHome && <NavBarHome />}
-      <Outlet />
-    </>
-  );
+  return  <Outlet />;
 }
 
 export default App;
